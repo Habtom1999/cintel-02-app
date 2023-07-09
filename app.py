e0565e25ba79e599576cff5d4d3105cb245cb788
elsagis <elsagis@yahoo.com>
2023-07-08 22:14:20 -0500

    Commit

--- app.py
+++ app.py
@@ -35,15 +35,15 @@ app_ui = ui.page_navbar(
         ),
     ),
     # TODO: Update the links to reflect your own about, GitHub repo, and app
-    ui.nav(ui.a("About", href="https://github.com/denisecase")),
-    ui.nav(ui.a("GitHub", href="https://github.com/denisecase/cintel-02-app")),
-    ui.nav(ui.a("App", href="https://denisecase.github.io/cintel-02-app/")),
+    ui.nav(ui.a("About", href="https://github.com/Habtom1999")),
+    ui.nav(ui.a("GitHub", href="https://github.com/Habtom1999/cintel-02-app")),
+    ui.nav(ui.a("App", href="https://Habtom1999.github.io/cintel-02-app/")),
     ui.nav(ui.a("Shiny", href="https://shiny.posit.co/py/")),
     ui.nav(ui.a("Examples", href="https://shinylive.io/py/examples/")),
     ui.nav(ui.a("Themes", href="https://rstudio.github.io/py-shinyswatch/")),
     ui.nav(ui.a("Deploy", href="https://docs.posit.co/shinyapps.io/getting-started.html#working-with-shiny-for-python")),
     # TODO: Update the title to reflect yourname Dashboard
-    title=ui.h1("Case Dashboard"),
+    title=ui.h1("Habtom Dashboard"),
 )
 
 
@@ -62,7 +62,7 @@ def server(input, output, session):
     @render.text
     def welcome_output():
         user = input.name_input();
-        welcome_string = f'Greetings {user}!';
+        welcome_string = f'Hello Class {user}!';
         return welcome_string
 
     @output
